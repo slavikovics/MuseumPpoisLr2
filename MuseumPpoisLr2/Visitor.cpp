515dcafcdcda9819ebcfdb64300ae172d55c961c
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-10-26 23:32:22 +0300

    Visitor Editing

--- MuseumPpoisLr2/Visitor.cpp
+++ MuseumPpoisLr2/Visitor.cpp
@@ -1,1 +1,14 @@
 #include "Visitor.h"
+
+namespace MuseumNamespace
+{
+	void Visitor::BrakeExhibit()
+	{
+		_hasBrokenExhibit = true;
+	}
+
+	void Visitor::BuySouvenir()
+	{
+		_hasBoughtSouvenir = true;
+	}
+}
