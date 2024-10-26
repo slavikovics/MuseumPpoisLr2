34eaa5542b7c12da2bf7209228f0c41acbfdc2f4
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-10-26 21:32:50 +0300

    Adding Employee realization

--- MuseumPpoisLr2/Employee.cpp
+++ MuseumPpoisLr2/Employee.cpp
@@ -1,1 +1,31 @@
 #include "Employee.h"
+
+namespace MuseumNamespace
+{
+	void Employee::IteractionWithVisitor()
+	{
+
+	}
+
+	Jobs Employee::GetJob()
+	{
+		return _job;
+	}
+
+	Employee::Employee(std::string name, int id)
+	{
+		_name = name;
+		_id = id;
+		_job = NoJob;
+	}
+
+	int Employee::GetId()
+	{
+		return _id;
+	}
+
+	std::string Employee::GetName()
+	{
+		return _name;
+	}
+}
