320c1dcffc8136e523da1b55e811355584e62db8
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-10-26 21:26:43 +0300

    Adding IPerson abstract class

---
+++ MuseumPpoisLr2/IPerson.h
@@ -0,0 +1,25 @@
+#ifndef IPERSON_H
+#define IPERSON_H
+
+#include <string>
+
+namespace MuseumNamespace
+{
+	class IPerson
+	{
+	protected:
+
+		std::string _name;
+
+		int _id;
+
+	public:
+
+		virtual std::string GetName() = 0;
+
+		virtual int GetId() = 0;
+	};
+}
+
+#endif
+
